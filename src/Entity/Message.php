2fca5b5da59aeb7cc89693fe44258b30021a96d6
wade199 <ibrahima97wade99@gmail.com>
2023-10-04 11:49:39 +0200

    premier commit

---
+++ src/Entity/Message.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessageRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\MetaData\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+
+#[ORM\Entity(repositoryClass: MessageRepository::class)]
+
+#[ApiResource(paginationItemsPerPage: 40,
+operations:[new Get (normalizationContext:['groups' => 'message:items']),
+      new GetCollection(normalizationContext: ['groups' => 'message:list'])]
+)]
+class Message
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    #[Groups(['message:list', 'mesage:item'])]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $datePoste = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['message:list', 'message:item'])]
+    private ?string $contenu = null;
+
+    #[ORM\ManyToOne(inversedBy: 'messages')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\ManyToOne(targetEntity: self::class, inversedBy: 'messages')]
+    private ?self $parent = null;
+
+    #[ORM\OneToMany(mappedBy: 'parent', targetEntity: self::class)]
+    private Collection $messages;
+
+    public function __construct()
+    {
+        $this->messages = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDatePoste(): ?\DateTimeInterface
+    {
+        return $this->datePoste;
+    }
+
+    public function setDatePoste(\DateTimeInterface $datePoste): static
+    {
+        $this->datePoste = $datePoste;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): static
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getParent(): ?self
+    {
+        return $this->parent;
+    }
+
+    public function setParent(?self $parent): static
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, self>
+     */
+    public function getMessages(): Collection
+    {
+        return $this->messages;
+    }
+
+    public function addMessage(self $message): static
+    {
+        if (!$this->messages->contains($message)) {
+            $this->messages->add($message);
+            $message->setParent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMessage(self $message): static
+    {
+        if ($this->messages->removeElement($message)) {
+            // set the owning side to null (unless already changed)
+            if ($message->getParent() === $this) {
+                $message->setParent(null);
+            }
+        }
+
+        return $this;
+    }
+}
